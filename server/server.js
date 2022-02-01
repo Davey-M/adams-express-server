8965b6bd5caced5b182e2950bf74083154a04eb1
Davey-M <davey.meuer@gmail.com>
2022-02-01 15:51:32 -0600

    Added quotes route to server

--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@ const app = express();
 
 const port = 3000;
 
+const quotes = require('./modules/quotes');
+
 // dir for static files
 app.use(express.static(__dirname + "/public"));
+
+// route should be a noun and describe the data you are getting from it
+app.get('/quotes', (req, res) => {
+    res.send(quotes);
+})
 
 app.listen(port, () => {
 	try {
