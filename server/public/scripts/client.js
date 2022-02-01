8c0e666619d00a39ac471b5196b88065e6de8f32
Davey-M <davey.meuer@gmail.com>
2022-02-01 15:54:37 -0600

    Added quotes to homepage

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,23 @@
 $(main)
 
-function main() {
+async function main() {
     console.log('Javascript and JQuery are loaded');
+
+    let quotes = await getQuotes();
+
+    for (let quote of quotes) {
+        $('#quoteContainer').append(`
+            <div>
+                <p>By: ${quote.author}</p>
+                <p>Quote: ${quote.text}</p>
+            </div>
+        `);
+    }
+}
+
+async function getQuotes() {
+    let response = await fetch('/quotes');
+    let quotes = await response.json();
+
+    return quotes;
 }
