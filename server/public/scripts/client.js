f943c3427e0b35e3136dd51082586d6db72a314c
Davey-M <davey.meuer@gmail.com>
2022-02-02 11:12:42 -0600

    Added new way of sourcing in quotes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -27,9 +27,51 @@ $(onReady)
 function onReady() {
     console.log('JS & JQuery');
 
-    getQuotes();
+    // getQuotes();
+    asyncGetQuotes();
 }
 
 function getQuotes() {
     // get quotes from the server with JQuery
+
+    $.ajax({
+        method: 'GET',
+        url: '/ruotes',
+    }).then(quotes => {
+        for (let quote of quotes) {
+            $('#quoteContainer').append(`
+                <div>
+                    <h3>By: ${quote.author}</h3>
+                    <p>Quote: ${quote.text}</p>
+                </div>
+            `);
+        }
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+async function asyncGetQuotes() {
+
+    try {
+
+        let quotes = await $.ajax({
+            method: 'GET',
+            url: '/quotes',
+        });
+
+        for (let quote of quotes) {
+            $('#quoteContainer').append(`
+                <div>
+                    <h3>By: ${quote.author}</h3>
+                    <p>Quote: ${quote.text}</p>
+                </div>
+            `);
+        }
+
+    } catch(err) {
+
+        console.error(err.responseText);
+    }
+
 }
